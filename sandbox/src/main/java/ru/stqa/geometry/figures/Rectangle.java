a29e34b11a409555485462e7eb3b194f771eb1b9
alexandrbogdanov <ironweed@list.ru>
2025-01-17 19:34:29 +0100

    Refactoring, code structured

--- sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
@@ -2,13 +2,15 @@ package ru.stqa.geometry.figures;
 
 public class Rectangle {
     public static void printRectangleArea(double a, double b) {
-        System.out.println("The area of a rectangle with sides "
-                + a + " and " + b + " is " + getProductOfTwo(a, b));
+        String text = String.format("The area of a rectangle with sides %f and %f" +
+                " is %f%n", a, b, getProductOfTwo(a, b));
+        System.out.printf(text);
     }
 
     public static void printRectangleArea(int a, int b) {
-        System.out.println("The area of a rectangle with sides "
-                + a + " and " + b + " is " + getProductOfTwo(a, b));
+        String text = String.format("The area of a rectangle with sides %d and %d" +
+                " is %d", a, b, getProductOfTwo(a, b));
+        System.out.printf(text);
     }
 
     private static int getProductOfTwo(int a, int b) {
--- sandbox/src/main/java/ru/stqa/geometry/figures/Square.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Square.java
@@ -2,9 +2,9 @@ package ru.stqa.geometry.figures;
 
 public class Square {
     public static void printSquareArea(int side) {
-        System.out.
-                println("The area of a square with a side "
-                        + side + " is " + geSquare(side));
+        String text = String.format("The area of a square with a side %d = %d%n",
+                side, geSquare(side));
+        System.out.printf(text);
     }
 
     public static int geSquare(int a) {
