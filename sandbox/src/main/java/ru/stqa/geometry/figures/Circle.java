3bc5f14d4c7c26abf14ed10d08615a89507275c0
alexandrbogdanov <ironweed@list.ru>
2025-01-24 19:07:22 +0100

    Circle constructor and tests implemented

--- sandbox/src/main/java/ru/stqa/geometry/figures/Circle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Circle.java
@@ -3,6 +3,19 @@ package ru.stqa.geometry.figures;
 import static java.lang.Math.PI;
 
 public record Circle(double radius) implements GeometricFigure {
+
+    public Circle {
+        validateRadius(radius);
+    }
+
+    private void validateRadius(double radius) {
+        if (radius <= 0) {
+            throw new IllegalArgumentException(String.format(
+                    "Radius of a circle must be positive: r=%.1f", radius));
+        }
+    }
+
+
     public double getArea() {
         return PI * this.radius * this.radius;
     }
--- sandbox/src/test/java/ru/stqa/geometry/figures/CircleTests.java
+++ sandbox/src/test/java/ru/stqa/geometry/figures/CircleTests.java
@@ -1,8 +1,11 @@
 package ru.stqa.geometry.figures;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CircleTests {
     Circle c = new Circle(5.0);
@@ -17,5 +20,18 @@ public class CircleTests {
     void canCalculatePerimeter() {
         var result = c.getPerimeter();
         assertEquals(31.4, result, 0.1);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "-5.0, 'Radius of a circle must be positive: r=-5.0'",
+            "-0.0, 'Radius of a circle must be positive: r=-0.0'"
+    })
+    void canNotHaveNegativeOrZeroRadius(double radius, String expectedMessage) {
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> new Circle(radius)
+        );
+        assertEquals(expectedMessage, exception.getMessage());
     }
 }
