4a29adbfe52eaaa3ad748a604bcee8ba8c72272e
alexandrbogdanov <ironweed@list.ru>
2025-01-24 12:28:01 +0100

    Exceptions, debugging

--- sandbox/src/main/java/Hello.java
+++ sandbox/src/main/java/Hello.java
@@ -1,5 +1,24 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        try {
+            String z = calculate();
+            System.out.println("Hello, World!");
+
+        } catch (ArithmeticException e) {
+            System.out.println("Caught an exception: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+    }
+
+    private static String calculate() {
+        var x = 1;
+        var y = 0;
+        int z = divide(x, y);
+        return String.format("The result of division of %d by %d is %d%n", x, y, z);
+    }
+
+    private static int divide(int x, int y) {
+        return x / y;
     }
 }
