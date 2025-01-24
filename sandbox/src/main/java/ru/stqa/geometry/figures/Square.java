43bcf7604ca8f91e747280ed917baf723fd9ec55
alexandrbogdanov <ironweed@list.ru>
2025-01-24 14:06:18 +0100

    Constructor for Square with throwing exception created, text checking for negative square side implemented

--- sandbox/src/main/java/ru/stqa/geometry/figures/Square.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Square.java
@@ -1,6 +1,14 @@
 package ru.stqa.geometry.figures;
 
 public record Square(double side) implements GeometricFigure {
+
+    public Square {
+        if (side < 0) {
+            throw new IllegalArgumentException("Side of a square can not be negative: " + side);
+        }
+    }
+
+
     public double getArea() {
         return this.side * this.side;
     }
--- sandbox/src/test/java/ru/stqa/geometry/figures/SquareTests.java
+++ sandbox/src/test/java/ru/stqa/geometry/figures/SquareTests.java
@@ -3,6 +3,7 @@ package ru.stqa.geometry.figures;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class SquareTests {
     Square s = new Square(5.0);
@@ -17,5 +18,15 @@ public class SquareTests {
     void canCalculatePerimeter() {
         var result = s.getPerimeter();
         assertEquals(20.0, result);
+    }
+
+    @Test
+    void canNotHaveNegativeSide() {
+        try {
+            new Square(-5.0);
+            fail("Expected IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            assertEquals("Side of a square can not be negative: -5.0", e.getMessage());
+        }
     }
 }
