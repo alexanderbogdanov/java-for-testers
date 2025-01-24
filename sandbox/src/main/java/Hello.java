e6cd956c39be8de50cc9bbc73a4b864fe203d69e
alexandrbogdanov <ironweed@list.ru>
2025-01-24 13:55:06 +0100

    Exception changed with if-else

--- sandbox/src/main/java/Hello.java
+++ sandbox/src/main/java/Hello.java
@@ -1,21 +1,14 @@
 public class Hello {
     public static void main(String[] args) {
-        try {
-            String z = calculate();
+            var x = 1;
+            var y = 1;
+            if (y == 0) {
+                System.out.println("Can't divide by zero");
+            } else {
+                var z = divide(x, y);
+                System.out.println(z);
+            }
             System.out.println("Hello, World!");
-
-        } catch (ArithmeticException e) {
-            System.out.println("Caught an exception: " + e.getMessage());
-            e.printStackTrace();
-        }
-
-    }
-
-    private static String calculate() {
-        var x = 1;
-        var y = 0;
-        int z = divide(x, y);
-        return String.format("The result of division of %d by %d is %d%n", x, y, z);
     }
 
     private static int divide(int x, int y) {
