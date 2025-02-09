bf557653ff4c9de44df7635a32c253d1db1bffb8
alexandrbogdanov <ironweed@list.ru>
2025-02-09 19:34:03 +0100

    GroupDeletion test refactored

--- addressbook_web_tests/src/test/java/ru/stqa/addressbook/tests/GroupDeletionTests.java
+++ addressbook_web_tests/src/test/java/ru/stqa/addressbook/tests/GroupDeletionTests.java
@@ -3,6 +3,8 @@ package ru.stqa.addressbook.tests;
 import org.junit.jupiter.api.Test;
 import ru.stqa.addressbook.model.GroupData;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class GroupDeletionTests extends TestBase {
 
 
@@ -11,7 +13,10 @@ public class GroupDeletionTests extends TestBase {
         if (!app.groups().isGroupPresent()) {
             app.groups().createGroup(new GroupData("group name", "group header", "group footer"));
         }
+        int groupCount = app.groups().getCount();
         app.groups().deleteGroup();
+        int newGroupCount = app.groups().getCount();
+        assertEquals(groupCount - 1, newGroupCount);
     }
 
 }
