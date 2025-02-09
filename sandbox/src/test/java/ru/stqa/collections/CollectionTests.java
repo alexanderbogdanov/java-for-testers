412e9a61f4e045bcd480d4642f7b69c7ff7f8943
alexandrbogdanov <ironweed@list.ru>
2025-02-09 01:10:24 +0100

    Collections ex-s 2: ArrayList

--- sandbox/src/test/java/ru/stqa/collections/CollectionTests.java
+++ sandbox/src/test/java/ru/stqa/collections/CollectionTests.java
@@ -3,6 +3,11 @@ package ru.stqa.collections;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CollectionTests {
 
     @Test
@@ -13,10 +18,36 @@ public class CollectionTests {
         array[1] = "two";
         array[2] = "three";
 
-        Assertions.assertEquals(3, array.length);
-        Assertions.assertEquals("one", array[0]);
+        assertEquals(3, array.length);
+        assertEquals("one", array[0]);
 
         array[0] = "five";
-        Assertions.assertEquals("five", array[0]);
+        assertEquals("five", array[0]);
+    }
+
+
+    @Test
+    void listTests() {
+        var list = new ArrayList<String>(); // Immutable
+        list.add("one");
+        list.add("two");
+        list.add("three");
+        assertEquals(3, list.size());
+        assertEquals("one", list.get(0));
+
+        list.set(0, "five");
+        assertEquals("five", list.get(0));
+
+        var list2 = List.of("one", "two", "three"); // Immutable, fixed size
+        assertEquals(3, list2.size());
+        assertEquals("one", list2.get(0));
+
+//        list2.set(0, "five"); // UnsupportedOperationException
+
+        var list3 = new ArrayList<>(List.of("one", "two", "three")); // Mutable
+        list3.set(0, "five");
+        assertEquals("five", list3.get(0));
+
+
     }
 }
