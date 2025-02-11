97396cc8dda05860691c70fd3bc296259ed53f77
alexandrbogdanov <ironweed@list.ru>
2025-02-11 18:21:16 +0100

    GroupCreationTests refactored, got rid of all tests and replaced with one parameterized test

--- addressbook_web_tests/src/test/java/ru/stqa/addressbook/tests/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/ru/stqa/addressbook/tests/GroupCreationTests.java
@@ -2,42 +2,37 @@ package ru.stqa.addressbook.tests;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import ru.stqa.addressbook.model.GroupData;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GroupCreationTests extends TestBase {
 
 
+    public static List<GroupData> groupProvider() {
+        var result = new ArrayList<GroupData>(List.of(
+                new GroupData(),
+                new GroupData().withName("some name"),
+                new GroupData("group name", "group header", "group footer"),
+                new GroupData("group name 2", "group header 2", "group footer 2"),
+                new GroupData("group name 3", "group header 3", "group footer 3")));
+        for (int i = 0; i < 5; i++) {
+            result.add(new GroupData(randomString(i * 10), randomString(i * 10), randomString(i * 10)));
+        }
+        return result;
+    }
+
     @ParameterizedTest
-    @ValueSource(strings = {"group name", "group name 2", "group name 3"})
-    public void testGroupCreation(String name) {
+    @MethodSource("groupProvider")
+    public void testMultipleGroupCreation(GroupData group) {
         int groupCount = app.groups().getCount();
-        app.groups().createGroup(new GroupData(name, "group header", "group footer"));
+        app.groups().createGroup(group);
         int newGroupCount = app.groups().getCount();
         assertEquals(groupCount + 1, newGroupCount);
-    }
-
-    @Test
-    public void testGroupCreationWithBlankFields() {
-        app.groups().createGroup(new GroupData());
-    }
-
-    @Test
-    public void testGroupCreationWithNameOnly() {
-        app.groups().createGroup(new GroupData().withName("some name"));
-    }
-
-    @Test
-    public void testMultipleGroupCreation() {
-        int n = 10;
-        int groupCount = app.groups().getCount();
-        for (int i = 0; i < n; i++) {
-            app.groups().createGroup(new GroupData(randomString(i * 10), "group header", "group footer"));
-        }
-        int newGroupCount = app.groups().getCount();
-        assertEquals(groupCount + n, newGroupCount);
     }
 
 }
