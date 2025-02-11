1a8e9086e1f755716f6f8d7ef2635faac7b7ada0
alexandrbogdanov <ironweed@list.ru>
2025-02-11 15:30:17 +0100

    First group creation test parameterized

--- addressbook_web_tests/src/test/java/ru/stqa/addressbook/tests/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/ru/stqa/addressbook/tests/GroupCreationTests.java
@@ -1,6 +1,8 @@
 package ru.stqa.addressbook.tests;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import ru.stqa.addressbook.model.GroupData;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -8,10 +10,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class GroupCreationTests extends TestBase {
 
 
-    @Test
-    public void testGroupCreation() {
+    @ParameterizedTest
+    @ValueSource(strings = {"group name", "group name 2", "group name 3"})
+    public void testGroupCreation(String name) {
         int groupCount = app.groups().getCount();
-        app.groups().createGroup(new GroupData("group name", "group header", "group footer"));
+        app.groups().createGroup(new GroupData(name, "group header", "group footer"));
         int newGroupCount = app.groups().getCount();
         assertEquals(groupCount + 1, newGroupCount);
     }
@@ -31,7 +34,6 @@ public class GroupCreationTests extends TestBase {
         int n = 10;
         int groupCount = app.groups().getCount();
         for (int i = 0; i < n; i++) {
-//            app.groups().createGroup(new GroupData("group name", "group header", "group footer"));
             app.groups().createGroup(new GroupData(randomString(i * 10), "group header", "group footer"));
         }
         int newGroupCount = app.groups().getCount();
