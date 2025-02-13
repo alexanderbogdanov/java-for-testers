7125f33695caaf122783fe09a45301cbdb8c0c00
alexandrbogdanov <ironweed@list.ru>
2025-02-13 15:23:59 +0100

    ContactCreationTests parameterized

--- addressbook_web_tests/src/test/java/ru/stqa/addressbook/tests/ContactCreationTests.java
+++ addressbook_web_tests/src/test/java/ru/stqa/addressbook/tests/ContactCreationTests.java
@@ -1,34 +1,64 @@
 package ru.stqa.addressbook.tests;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import ru.stqa.addressbook.model.ContactData;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ContactCreationTests extends TestBase {
 
-    @Test
-    public void testContactCreation() {
-        app.contacts().createContact(
-                new ContactData(
-                        "Benedict",
-                        "Cumberbatch",
-                        "221B Baker Street, London, NW1 6XE",
-                        "+44 20 7900 9000",
-                        "+44 7700 900900",
-                        "+44 20 7946 0000",
-                        "benedict.cumberbatch@example.com",
-                        "ben.cumberbatch@example.net",
-                        "benny.cumberbatch@example.co.uk"));
+    public static List<ContactData> contactProvider() {
+        var result = new ArrayList<ContactData>();
 
+        result.add(new ContactData("Benedict", "Cumberbatch", "221B Baker Street",
+                "+44 20 7900 9000", "+44 7700 900900", "+44 20 7946 0000",
+                "benedict@example.com", "ben.c@example.net", "benny@example.co.uk"));
+
+        result.add(new ContactData());
+
+        result.add(new ContactData().withFirstName("John"));
+        result.add(new ContactData().withLastName("Doe"));
+        result.add(new ContactData().withEmail("johndoe@example.com"));
+
+        for (int i = 1; i <= 5; i++) {
+            result.add(new ContactData(
+                    randomString(5), randomString(5), randomString(10),
+                    randomString(10), randomString(10), randomString(10),
+                    randomString(10), randomString(10), randomString(10)
+            ));
+        }
+
+        return result;
     }
 
-    @Test
-    public void testContactCreationWithBlankFields() {
-        app.contacts().createContact(new ContactData());
+
+    @ParameterizedTest
+    @MethodSource("contactProvider")
+    public void testMultipleContactCreation(ContactData contact) {
+        int contactCount = app.contacts().getCount();
+        app.contacts().createContact(contact);
+        int newContactCount = app.contacts().getCount();
+        assertEquals(contactCount + 1, newContactCount);
     }
 
-    @Test
-    public void testContactCreationWithFirstNameOnly() {
-        app.contacts().createContact(new ContactData().withFirstName("Benedict"));
+
+    public static List<ContactData> NegativeContactProvider() {
+        return new ArrayList<>(List.of(
+                new ContactData("", "last name'", "",
+                        "", "", "", "", "", "")));
+    }
+
+    @ParameterizedTest
+    @MethodSource("NegativeContactProvider")
+    public void testContactCreationFail(ContactData contact) {
+        int contactCount = app.contacts().getCount();
+        app.contacts().createContact(contact);
+        int newContactCount = app.contacts().getCount();
+        assertEquals(contactCount, newContactCount);
     }
 
 }
