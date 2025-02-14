b32c4d970b9509ac2020add7845f211d6ece7d25
alexandrbogdanov <ironweed@list.ru>
2025-02-14 12:08:34 +0100

    GoupModificationTests refactored to sort and compare lists

--- addressbook_web_tests/src/test/java/ru/stqa/addressbook/tests/GroupModificationTests.java
+++ addressbook_web_tests/src/test/java/ru/stqa/addressbook/tests/GroupModificationTests.java
@@ -3,6 +3,12 @@ package ru.stqa.addressbook.tests;
 import org.junit.jupiter.api.Test;
 import ru.stqa.addressbook.model.GroupData;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class GroupModificationTests extends TestBase {
 
     @Test
@@ -10,6 +16,18 @@ public class GroupModificationTests extends TestBase {
         if (app.groups().getCount() == 0) {
             app.groups().createGroup(new GroupData("", "group name", "group header", "group footer"));
         }
-        app.groups().modifyGroup(new GroupData().withName("modified name"));
+        var groupsBefore = app.groups().getAll();
+        var index = new Random().nextInt(groupsBefore.size());
+        var testData = new GroupData().withName("modified name");
+        app.groups().modifyGroup(groupsBefore.get(index), testData);
+        var groupsAfter = app.groups().getAll();
+        var expectedGroups = new ArrayList<>(groupsBefore);
+        expectedGroups.set(index, testData.withId(groupsBefore.get(index).id()));
+        Comparator<GroupData> compareById = (g1, g2) -> {
+            return Integer.compare(Integer.parseInt(g1.id()), Integer.parseInt(g2.id()));
+        };
+        groupsAfter.sort(compareById);
+        expectedGroups.sort(compareById);
+        assertEquals(groupsAfter, expectedGroups);
     }
 }
