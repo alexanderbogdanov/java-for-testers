4da5edfbfb506c88a5e779c4188418ea834ca2a2
alexandrbogdanov <ironweed@list.ru>
2025-03-02 00:32:23 +0100

    file ex

--- sandbox/src/main/java/Hello.java
+++ sandbox/src/main/java/Hello.java
@@ -1,3 +1,5 @@
+import java.io.File;
+
 public class Hello {
     public static void main(String[] args) {
             var x = 1;
@@ -9,6 +11,10 @@ public class Hello {
                 System.out.println(z);
             }
             System.out.println("Hello, World!");
+            var configFile = new File("sandbox/build.gradle");
+        System.out.println(configFile.getAbsolutePath());
+        System.out.println(configFile.exists());
+        System.out.println(new File("").getAbsolutePath());
     }
 
     private static int divide(int x, int y) {
